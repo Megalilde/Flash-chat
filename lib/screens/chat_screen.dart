109fea6fb285549ededb4660513505b51152186c
Megalilde <63415418+Megalilde@users.noreply.github.com>
2022-04-29 15:00:29 +0200

    Added retrive data from firebase in realtime.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatScreen extends StatefulWidget {
 
   static const String id = 'chat_screen';
@@ -11,16 +13,34 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-
+  String messageText = '';
+  late User? user;
 
   void getCurrentUser() async{
-    final user = await _auth.currentUser;
+    user = await _auth.currentUser;
     if(user != null){
-    print(user.email);
+    print(user?.email);
     }
   }
+  
+  /*void getMessages()async{
+    final messages = await _firestore.collection('messages').get();
+    for(var message in messages.docs){
+      print(message.data());
+    }
 
+  }*/
+
+  void messagesStream() async{
+    //con snapshot ascolto le modifiche della mia raccolta appena avviene un cambiamento nel database.
+    await for(var snapshot in _firestore.collection('messages').snapshots()){
+      for(var message in snapshot.docs){
+        print(message.data());
+      }
+    }
+  }
 
   @override
   void initState() {
@@ -36,9 +56,11 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
+                //getMessages(); <- per stampare
+                messagesStream();
                 //Implement logout functionality
-                _auth.signOut();
-                Navigator.pop(context);
+                //_auth.signOut();
+                //Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -49,6 +71,30 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot){
+                if(snapshot.hasData){
+                  final messages = snapshot.data?.docs;
+                  List<Text> messageWidgets = [];
+                  for(var message in messages!){
+                    final messageText = message.get('text');
+                    final messageSender = message.get('sender');
+                    final messageWidget = Text('$messageText from $messageSender');
+                    messageWidgets.add(messageWidget);
+                  }
+                  return Column(
+                    children: messageWidgets,
+                  );
+                }else{
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -58,6 +104,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: TextField(
                       onChanged: (value) {
                         //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
@@ -65,6 +112,11 @@ class _ChatScreenState extends State<ChatScreen> {
                   FlatButton(
                     onPressed: () {
                       //Implement send functionality.
+                      //messageText + user.email
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': user?.email,
+                      });
                     },
                     child: Text(
                       'Send',
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'chat_screen.dart';
 
